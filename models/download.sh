8008dbcea492f51407683f4674097c635a1c022c
Sergio Rodenas <sergiorodenasgomez@gmail.com>
2024-08-21 13:45:06 +0200

    WIP

---
+++ models/download.sh
@@ -0,0 +1,3 @@
+wget -q -O lazymix.safetensors https://civitai.com/api/download/models/302254 &&
+wget -q -O sam_hq_vit_l.pth https://huggingface.co/lkeab/hq-sam/resolve/main/sam_hq_vit_l.pth &&
+wget -q -O groundingdino_swint_ogc.pth https://huggingface.co/ShilongLiu/GroundingDINO/resolve/main/groundingdino_swint_ogc.pth
